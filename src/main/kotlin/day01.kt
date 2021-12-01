efa2cbc302c67d2b2f5a97ff8e7082f5b9686c53
Mathias Henze <mathias.henze@exaring.de>
2021-12-01 13:32:32 +0100

    skipping sliding window sum, since it is not necessary

--- src/main/kotlin/day01.kt
+++ src/main/kotlin/day01.kt
@@ -2,22 +2,19 @@ import java.lang.ClassLoader.getSystemResource
 import java.lang.Integer.parseInt
 
 fun day01Puzzle1() {
-    val increments = day1Input
-        .windowed(2)
-        .filter { it[0] < it[1] }
-        .size
-    println("Day  1, Puzzle 1: $increments increments")
+    println("Day  1, Puzzle 1: ${day1Input.countIncrements()} increments")
 }
 
 fun day01Puzzle2() {
-    val increments = day1Input
-        .windowed(3)
-        .map { it.sum() }
-        .windowed(2)
-        .filter { it[0] < it[1] }
-        .size
-    println("Day  1, Puzzle 2: $increments increments")
+    println("Day  1, Puzzle 2: ${day1Input.countIncrements(3)} increments")
 }
+
+private fun List<Int>.countIncrements(slidingWindow: Int = 1) = this
+    // since with the sliding window, all elements but the first and the last are overlapping, we can simplify this
+    // to just compare the first and the last element and skip the sum
+    .windowed(slidingWindow + 1)
+    .filter { it.first() < it.last() }
+    .size
 
 private val day1Input get() = getSystemResource("day01_input.txt")
     .readText()
