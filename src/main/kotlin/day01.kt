4f91baac0264cd708fcc8033d4ab022d407fd26f
Mathias Henze <mathias.henze@exaring.de>
2021-12-01 13:08:41 +0100

    adding puzzle 2

--- src/main/kotlin/day01.kt
+++ src/main/kotlin/day01.kt
@@ -2,13 +2,25 @@ import java.lang.ClassLoader.getSystemResource
 import java.lang.Integer.parseInt
 
 fun day01Puzzle1() {
-    val increments = getSystemResource("day01_puzzle1_input.txt")
-        .readText()
-        .lines()
-        .filter { it.isNotBlank() }
-        .map { parseInt(it) }
+    val increments = day1Input
         .windowed(2)
         .filter { it[0] < it[1] }
         .size
     println("Day  1, Puzzle 1: $increments increments")
 }
+
+fun day01Puzzle2() {
+    val increments = day1Input
+        .windowed(3)
+        .map { it.sum() }
+        .windowed(2)
+        .filter { it[0] < it[1] }
+        .size
+    println("Day  1, Puzzle 2: $increments increments")
+}
+
+private val day1Input get() = getSystemResource("day01_input.txt")
+    .readText()
+    .lines()
+    .filter { it.isNotBlank() }
+    .map { parseInt(it) }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,5 @@
 
 fun main(args: Array<String>) {
     day01Puzzle1()
+    day01Puzzle2()
 }
