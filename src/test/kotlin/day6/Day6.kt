fbd6336dad65d63de3689e4fd8f8b0eb6f545481
Mathias Henze <mathias.henze@exaring.de>
2021-12-06 12:01:08 +0100

    day 6

---
+++ src/test/kotlin/day6/Day6.kt
@@ -0,0 +1,38 @@
+package day6
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day6 {
+
+    val sample = parse("sample.txt")
+    val input = parse("input.txt")
+
+    @Test
+    fun puzzle1() {
+        sample.population(days = 80) shouldBe 5934
+        println("Day  6, Puzzle 1: ${input.population(days = 80)} laternfish")
+    }
+
+    @Test
+    fun puzzle2() {
+        sample.population(days = 256) shouldBe 26984457539
+        println("Day  6, Puzzle 2: ${input.population(days = 256)} laternfish")
+    }
+
+    fun List<Int>.population(days: Int) = map { it.withOffspring(days) }.sum()
+
+    // modified iterative fibonacci
+    fun Int.withOffspring(days: Int) = (1..days - this)
+        .fold(listOf(1L) + List(8) { 0L }) { sea, _ ->
+            sea.slice(1..6) + (sea[7] + sea.first()) + sea.last() + sea.first()
+        }
+        .sum()
+
+    fun parse(resource: String) = this.javaClass
+        .getResource(resource)
+        .readText()
+        .trim()
+        .split(Regex("\\D+"))
+        .map { it.toInt() }
+}
