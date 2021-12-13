abc6a0e1fd8eca2d467e3cfde0ee8c80437db70d
Mathias Henze <mathias.henze@exaring.de>
2021-12-13 09:15:37 +0100

    day 13

---
+++ src/test/kotlin/day13/Day13.kt
@@ -0,0 +1,76 @@
+package day13
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import kotlin.math.abs
+
+// poor mans point type
+typealias Point = Pair<Int, Int>
+
+val Point.x get() = first
+val Point.y get() = second
+
+class Day13 {
+    data class Input(val points: List<Point>, val folds: List<Point>)
+
+    val sample = parse("sample.txt")
+    val data = parse("data.txt")
+
+    @Test
+    fun part1() {
+        sample.dotsAfterFolds(1).count() shouldBe 17
+        println("Day 12, Part 1: ${data.dotsAfterFolds(1).count()} points")
+    }
+
+    @Test
+    fun part2() {
+        sample.dotsAfterFolds().plotted().trim() shouldBe """
+            #####
+            #   #
+            #   #
+            #   #
+            #####
+        """.trimIndent()
+        println(sample.dotsAfterFolds())
+        println("Day 12, Part 2:\n${data.dotsAfterFolds().plotted()}")
+    }
+
+    fun Input.dotsAfterFolds(numFolds: Int = folds.size) = points
+        .map {
+            (0 until numFolds).fold(it) { p, n -> Point(foldAt(folds[n].x, p.x), foldAt(folds[n].y, p.y)) }
+        }
+        .toSet()
+
+    fun Set<Point>.plotted(): String {
+        val max = Point(maxOf { it.x }, maxOf { it.y })
+        var sheet = ""
+        for (y in 0..max.y) {
+            for (x in 0..max.x) {
+                sheet += if (contains(Point(x, y))) "#" else " "
+            }
+            sheet += "\n"
+        }
+        return sheet
+    }
+
+    fun foldAt(f: Int, p: Int) = if (p > f) abs(f * 2 - p) else p
+
+    fun parse(resource: String) = this.javaClass.getResource(resource)
+        .readText()
+        .lines()
+        .filter { it.isNotBlank() }
+        .map { it.trim().split(Regex("\\W+")) }
+        .partition { it[0] != "fold" }
+        .let { segments ->
+            Input(
+                segments.first.map { Point(it[0].toInt(), it[1].toInt()) },
+                segments.second.map { if (it[2] == "x") Point(it[3].toInt(), 0) else Point(0, it[3].toInt()) }
+            )
+        }
+
+}
+
+fun main() = Day13().run {
+    part1()
+    part2()
+}
