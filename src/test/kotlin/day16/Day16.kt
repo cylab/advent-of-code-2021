fdc398b56b4e15014a3e68dbb1281365c5272487
Mathias Henze <mathias.henze@exaring.de>
2021-12-16 23:05:30 +0100

    cleanup

--- src/test/kotlin/day16/Day16.kt
+++ src/test/kotlin/day16/Day16.kt
@@ -6,7 +6,7 @@ import kotlin.Int.Companion.MAX_VALUE
 
 typealias Input = String
 
-typealias Biter = Iterator<Char>
+typealias BIter = Iterator<Char>
 
 class Day16 {
 
@@ -14,7 +14,7 @@ class Day16 {
         val version: Int,
         val typeId: Int,
         val literal: Long? = null,
-        val packets: List<Packet> = emptyList()
+        val subPackets: List<Packet> = emptyList()
     )
 
     val sample = parse("sample.txt")
@@ -22,7 +22,7 @@ class Day16 {
 
     @Test
     fun part1() {
-        unpack("38006F45291200".hexToBits().iterator())
+        "38006F45291200".hexToBits().iterator().unpack()
         sample.iterator().sumVersions() shouldBe 31
         println("Day  16, Part 1: ${data.iterator().sumVersions()} version sum")
     }
@@ -33,48 +33,41 @@ class Day16 {
 //        println("Day  16, Part 2: ${data.versionSum()} version sum")
     }
 
-    fun Biter.sumVersions() = unpack(this).flatMap { it.flattened() }.sumOf { it.version }
+    fun BIter.sumVersions() = unpack().first().withSubPackets().sumOf { it.version }
 
-    fun Packet.flattened(): List<Packet> = listOf(this) + packets.flatMap { it.flattened() }
-
-    fun unpack(bits: Biter, num: Int = 1): List<Packet> {
+    fun BIter.unpack(num: Int = 1): List<Packet> {
         val packets = mutableListOf<Packet>()
         for (c in 1..num) {
-            val version = bits.nextValue(3)
-            val typeId = bits.nextValue(3)
+            val version = nextValue(3)
+            val typeId = nextValue(3)
             if (typeId == 4) {
-                packets.add(Packet(version, typeId, literal = bits.nextLiteral()))
+                packets.add(Packet(version, typeId, literal = nextLiteral()))
             } else {
-                val lengthId = bits.nextValue()
-                val length = when (lengthId) {
-                    0 -> bits.nextValue(15)
-                    else -> bits.nextValue(11)
+                val lengthId = nextValue()
+                val subPackets = when (lengthId) {
+                    0 -> next(nextValue(15)).iterator().unpack(MAX_VALUE)
+                    else -> unpack(nextValue(11))
                 }
-                val packet = when (lengthId) {
-                    0 -> {
-                        val subPackets = bits.next(length)
-                        Packet(version, typeId, packets = unpack(subPackets.iterator(), MAX_VALUE))
-                    }
-                    else -> Packet(version, typeId, packets = unpack(bits, length))
-                }
-                packets.add(packet)
+                packets.add(Packet(version, typeId, subPackets = subPackets))
             }
-            if (!bits.hasNext())
+            if (!hasNext())
                 break
         }
         return packets
     }
 
-    fun Biter.next(num: Int) = (1..num).map { next() }.joinToString("")
-    fun Biter.nextValue(num: Int = 1) = next(num).toInt(2)
-    fun Biter.nextLiteral(): Long {
+    fun BIter.next(num: Int) = (1..num).map { next() }.joinToString("")
+    fun BIter.nextValue(num: Int = 1) = next(num).toInt(2)
+    fun BIter.nextLiteral(): Long {
         var bits = ""
         do {
-            val keepReading = next() == '1'
+            val keepReading = nextValue() == 1
             bits += next(4)
         } while (keepReading)
         return bits.toLong(2)
     }
+
+    fun Packet.withSubPackets(): List<Packet> = listOf(this) + subPackets.flatMap { it.withSubPackets() }
 
     fun String.hexToBits() = trim().flatMap {
         "%4s".format(it.toString().toInt(16).toString(2)).replace(' ', '0').toList()
